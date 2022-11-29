3242104cac707d5b207d4ea28a4ec9e7333ed59a
chloe2103 <nhushahoang0711@gmail.com>
2022-11-29 17:20:42 -0500

    first commit

---
+++ src/clas/DoWhileLoop.java
@@ -0,0 +1,18 @@
+package clas;
+
+public class DoWhileLoop {
+public static void main(String[]args){
+		
+		int count=1;//starting point(where to start)
+		
+		do {
+		 System.out.println(count);
+		count++;
+	
+		}while (count > 10); //maximum point
+			
+}
+}
+		 
+		//incrimintation(count ++ means +1.
+	
---
+++ src/clas/Loop.java
@@ -0,0 +1,15 @@
+package clas;
+
+public class Loop {
+	public static void main(String[]args) {
+		
+		int count=0;//starting point(where to start)
+		while (count <10) {//maximum point///conditio//if only the condition is >10
+		 System.out.println(count);
+		 
+		count++;//incrimintation(count ++ means +1.
+
+		}
+	}
+}
+	
